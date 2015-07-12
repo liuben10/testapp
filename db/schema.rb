db5c75b4748f641d3aeb82d7290a65bd558dcf95
bliu <ben.liu@xoom.com>
2015-07-11 18:39:56 -0700

    Change description to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711225447) do
+ActiveRecord::Schema.define(version: 20150712013709) do
 
   create_table "quests", force: true do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description", limit: 255
     t.integer  "rating"
     t.integer  "user_id"
     t.datetime "created_at"
